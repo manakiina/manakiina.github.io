107366197b3b04446360ef9204fdf52c81b72f22
manakiina <manakiina@yandex.ru>
2025-02-04 23:15:00 +0300

    Add files via upload

--- site1/static/dom_scripts.js
+++ site1/static/dom_scripts.js
@@ -1,7 +1,13 @@
-const lastName = document.getElementsByTagName('p')[0].childNodes[1].innerText;
-const firstName = document.getElementsByTagName('p')[1].childNodes[1].innerText;
-const birthYear = document.getElementsByTagName('p')[2].childNodes[1].innerText;
+const lastName = document.getElementsByTagName('p')[0].textContent;
+const firstName = document.getElementsByTagName('p')[1].textContent;
+const patronymic = document.getElementsByTagName('p')[2].textContent;
+const birthDate = document.getElementsByTagName('p')[3].textContent;
+const birthPlace = document.getElementsByTagName('p')[4].textContent;
+const issueDate = document.getElementsByTagName('p')[5].textContent;
 
 console.log(`Фамилия: ${lastName}`);
 console.log(`Имя: ${firstName}`);
-console.log(`Год рождения: ${birthYear}`);
+console.log(`Отчество: ${patronymic}`);
+console.log(`Дата рождения: ${birthDate}`);
+console.log(`Место рождения: ${birthPlace}`);
+console.log(`Дата выдачи: ${issueDate}`);
